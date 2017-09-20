575395331e6657aa2781de642ac511a22a8e0bf5
jeffreyshao <jeffreyshao18@gmail.com>
2017-09-19 22:28:31 -0400

    Takin the L

--- server/config/config.js
+++ server/config/config.js
@@ -1,13 +1,13 @@
-//This file holds any configuration variables we may need 
+//This file holds any configuration variables we may need
 //'config.js' is ignored by git to protect sensitive information, such as your database's username and password
 //copy this file's contents to another file 'config.js' and store your MongoLab uri there
 
 module.exports = {
   db: {
-    uri: '', //place the URI of your mongo database here.
-  }, 
+    uri: 'mongodb://jeffreyshao:qwerty1234@ds123084.mlab.com:23084/jeffreyshao', //place the URI of your mongo database here.
+  },
   googleMaps: {
-    key: ''
+    key: 'AIzaSyAlCyTa5XSllHh0VvBEaVrqGVM9fE4Dt8'
   },
   port: 8080
 };
